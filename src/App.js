e8fd8fcbd27f2acc795f34989f283e5364b94316
Agostina Romero <agostinaromontamatt@gmail.com>
2021-11-25 23:14:27 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a>
-          ShoStore
-        </a>
-      </header>
+        <Navbar/>
+
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+
+import './Navbar.css';
+
+          const Navbar=()=>{
+    return (
+      
+          <div className= 'Menu'>
+          <nav className="NavbarItems">
+            
+    <ul>
+      <li><a>Home</a></li>
+      <li><a>Men's</a></li>
+      <li><a>Woman's</a></li>
+      <li><a>Kid's</a></li>
+
+    </ul>
+</nav>
+          
+</div>
+    )
+  }
+
+export default Navbar;
