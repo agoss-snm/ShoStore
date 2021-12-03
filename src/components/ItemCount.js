91c8fcfa714af0abebfa113409c199f64efb5067
Agostina Romero <agostinaromontamatt@gmail.com>
2021-12-02 22:20:44 -0300

    cambios

---
+++ src/components/ItemCount.js
@@ -0,0 +1,42 @@
+
+import { Button } from 'semantic-ui-react';
+import React, {useState } from "react";
+
+const ItemCount=({stock, initial})=>{
+    const [counter, setCounter]= React.useState(initial);
+    
+    function handleIncrement() {
+        if(counter < stock){
+            setCounter(counter+1);
+        }else{
+            alert("no hay mas stock")
+        }
+
+    }
+    
+    function handleDecrement() {
+        if(counter == 0){
+            alert ("Carrito Vacio")
+        }else{
+        setCounter (counter -1);
+    }
+}
+
+    return (
+        <div className='ItemCount'>
+        <div className='ContadorContainer'>
+            
+            <h1>Cantidad: {counter}</h1>
+        
+           <Button onClick={handleDecrement}> -</Button>
+            <Button onClick={handleIncrement}> +</Button>
+
+           
+         </div>
+
+</div>
+        );
+    };
+
+    export default ItemCount; 
+	
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,13 @@
+import ItemCount from './ItemCount';
+
+
+
 const ItemListContainer=({greetings})=>{
     
     return (
         <div>
         {greetings}
+        <ItemCount stock={7} initial={0}/>
         </div>
     );
         };
