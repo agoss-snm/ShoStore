93c4a2340d8efcc811762a6469dc756540e13f69
Agostina Romero <agostinaromontamatt@gmail.com>
2021-12-21 21:27:52 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import AppRouter from './components/AppRouter/AppRouter';
-
+import Footer from './components/Footer/Footer'
 
 
 function App() {
   return (
     <div className="App">
        <AppRouter/>
+       <Footer/>
     </div>
   );
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,46 @@
+import './Footer.css';
+import React from 'react'
+import { Flag, Segment } from 'semantic-ui-react';
+import { Icon, Step } from 'semantic-ui-react'
+
+
+
+const Footer=()=>{
+    return (
+      
+    <div className='FooterContainer'>
+    <Segment>
+    <Flag name='ar' />
+  </Segment>
+
+  <Step.Group stackable className='tablet'>
+    <Step className='footerC'>
+      <Icon className='plane' />
+      <Step.Content>
+        <Step.Title>Shipping</Step.Title>
+        <Step.Description>Choose your shipping options</Step.Description>
+      </Step.Content>
+    </Step>
+    <Step active className='footerB'>
+      <Icon className='dollar' />
+      <Step.Content>
+        <Step.Title>Billing</Step.Title>
+        <Step.Description>Enter billing information</Step.Description>
+      </Step.Content>
+    </Step>
+    <Step active  className='footerA'>
+      <Icon className='info circle' />
+      <Step.Content>
+        <Step.Title>Confirm Order</Step.Title>
+        <Step.Description>Verify order details</Step.Description>
+      </Step.Content>
+    </Step>
+  </Step.Group>
+
+  <h3>All rights reserved <spam>@ShooStore</spam></h3>
+    </div>
+
+    )
+  }
+
+export default Footer;
