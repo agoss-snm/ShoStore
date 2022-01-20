05381b7aa2f47958179b73ecbbd885f9587d4e1b
Agostina Romero <agostinaromontamatt@gmail.com>
2022-01-20 18:06:09 -0300

    cambios

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -8,32 +8,38 @@ import './Item.css';
 const Item = ( {data}) => {
   return(  
     <Card.Group className='cardGroup'>
-      <Card className='col-4'>
+
+
+      <Link to={`item/${data.id}`}> <Card className='col-4'>
         <div className='imageS'>
-          <Link to='item/:id'><Image  className='Di' src= {`'../img/productos/'${data.image}`}/> </Link>
+          //
+          <Image  className='Di' src= {`'../img/productos/'${data.image}`}/> 
         </div>
 
         <Card.Meta>
-          <Link to='item/:id'><a><span className='titleD'>{data.name}</span></a></Link>
+          <a><span className='titleD'>{data.name}</span></a>
         </Card.Meta>
                 
         <Card.Content className='cardContent'>
-          <Link to='item/:id'>
+         
             <Card.Header className='price'> ${data.price} <span className='extra'> <Icon name='percent' />22  OFF</span> </Card.Header>
-          </Link>
+          
                 
         <Card.Description> 
-          <Link to={`item/${data.id}`}>  <a><span className='titleD'> <Button content='  Ver Producto' icon='right arrow' labelPosition='right' className='butonC'/> </span></a></Link>
-          <Link to=''>
+          <a><span className='titleD'> <Button content='  Ver Producto' icon='right arrow' labelPosition='right' className='butonC'/> </span></a>
+          
             <Button as='div' className='heartButton' labelPosition='center'>
             <Button color='red' ><Icon name='heart'  />Like</Button>
             <Label as='a'  basic color='red' pointing='left'>2,048</Label>
             </Button> 
-          </Link>
+          
         </Card.Description> 
         
         </Card.Content>
-        </Card>
+        </Card></Link>
+
+
+
       </Card.Group>
         )
     }
