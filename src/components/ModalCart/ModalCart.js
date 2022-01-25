88b088e77ddb123bd99a91dc2c5ea71c56e132d1
Agostina Romero <agostinaromontamatt@gmail.com>
2022-01-25 14:06:15 -0300

    cambios

--- src/components/ModalCart/ModalCart.js
+++ src/components/ModalCart/ModalCart.js
@@ -2,6 +2,16 @@ import React from "react";
 import {Link} from 'react-router-dom';
 //Semantic UI Imports
 import { Button} from 'semantic-ui-react';
+// material
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
 
 
 
@@ -15,16 +25,40 @@ return (
         {products.map((product)=>{
             return(
             <div className= 'item-cart-list'>
-                <p>{product.title}</p>
-                <span>{product.price} </span>
-                <span>Cant:{} </span>
+
+                <Table sx={{ minWidth: 300 }} aria-label="spanning table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" colSpan={3}>
+            Shopping list
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Unit</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow>
+              <TableCell align="right">{product.name}</TableCell>
+              <TableCell align="right">${product.price}</TableCell>
+              <TableCell align="right">4</TableCell>
+            </TableRow>
+
+          <TableRow>
+            <TableCell rowSpan={3} />
+            <TableCell colSpan={2}>TOTAL</TableCell>
+            <TableCell align="right">${total}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+
             </div>
             )
         })}
             <div>
-                <>
-                <p>total: <spam>${total}</spam></p>
-                </>
             <Button  onClick={cleanCart}>Clean Cart</Button>
             <Link to='/Cart'><Button>Finish</Button></Link>
             </div>
