76e9513d4df44cd61b37430a1bff44dee52569cd
Agostina Romero <agostinaromontamatt@gmail.com>
2024-05-06 16:30:43 -0300

    fixed

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,41 +1,41 @@
 import { Link } from "react-router-dom";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActions } from '@mui/material';
-import CardActionArea from '@mui/material/CardActionArea'; 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActions } from "@mui/material";
+import CardActionArea from "@mui/material/CardActionArea";
 import "./Item.css";
 
 const Item = ({ data }) => {
   return (
     <div className="container">
-          <Link to={`/item/${data.id}`}>
-          <Card sx={{ maxWidth: 245 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="200"
-          image={data.image}
-          alt={data.name}
+      <Link to={`/item/${data.id}`}>
+        <Card sx={{ maxWidth: 320 }}>
+          <CardActionArea>
+                   <CardMedia
+            component="img"
+            height="300" 
+            image={data.image}
+            alt={data.name}
+            style={{ objectFit: 'contain' }}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {data.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          $ {data.price}
-           
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          View
-        </Button>
-      </CardActions>
-    </Card>
-          </Link>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {data.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                $ {data.price}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              View
+            </Button>
+          </CardActions>
+        </Card>
+      </Link>
     </div>
   );
 };
